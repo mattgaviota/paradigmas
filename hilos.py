cbda897d797cfd76a72308a81d40952f9a10169e
mattgaviota <mattgaviota@gmail.com>
2011-11-30 20:16:48 -0300

    suma de lista con hilos

---
+++ hilos.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from random import randint
+from threading import Thread
+import time
+
+class Hilo(Thread):
+
+    def __init__(self, lista):
+        Thread.__init__(self)
+        self.lista = lista
+        self.suma = 0
+
+    def run(self):
+        self.status = sum(self.lista)
+
+
+def genera_lista(num):
+    lista = []
+    for x in xrange(num):
+        r = randint(0, 5000)
+        if r % 2 != 0:
+            lista.append(float(r))
+        else:
+            lista.append(r)
+        print '.',
+    return lista
+
+
+def divide_lista(lista, divisor):
+    otro = len(lista)/divisor
+    for i in xrange(divisor):
+        yield lista[otro*i:otro*(i+1)]
+
+def main():
+    longitud = raw_input('Ingrese la cantidad de elementos de la lista : ')
+    cantidad_de_hilos = raw_input('Ingrese la cantidad de hilos a ejecutar : ')
+    print "Generando lista de números aleatorios....."
+    lista = genera_lista(int(longitud))
+    divisiones = [y for y in divide_lista(lista, int(cantidad_de_hilos))]
+    print 'Iniciando suma de la lista de %s elementos' % (len(lista), )
+    s = time.time()
+    hilos = []
+    for listas in divisiones:
+         hilo = Hilo(listas)
+         hilo.start()
+         hilos.append(hilo)
+    suma = 0
+    for hilo in hilos:
+        #hilo.start()
+        hilo.join()
+        suma += hilo.status
+    print 'Suma con threads: '
+    print suma
+    print 'Tiempo total = %f' % (time.time() - s)
+    print 'Suma sin threads: '
+    t = time.time()
+    print sum(lista)
+    print 'Tiempo total = %f' % (time.time() - t)
+
+if __name__ == '__main__':
+    main()
